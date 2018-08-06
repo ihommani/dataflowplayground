f34899d14f4ceb4ed404e6b92d5a8e469da6c4b0
ihommani <ismaelhommani@gmail.com>
2018-08-06 16:34:17 +0200

    Implement wordcount
    Replaced the starter logic with the one of word counter

--- src/main/java/com/ihommani/dataflow/starter/StarterPipeline.java
+++ src/main/java/com/ihommani/dataflow/starter/StarterPipeline.java
@@ -3,18 +3,29 @@ package com.ihommani.dataflow.starter;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.metrics.Counter;
+import org.apache.beam.sdk.metrics.Distribution;
+import org.apache.beam.sdk.metrics.Metrics;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.Validation;
 import org.apache.beam.sdk.testing.PAssert;
-import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.joda.time.Duration;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * A starter example for writing Beam programs.
@@ -34,38 +45,112 @@ import java.util.Map;
  */
 @Slf4j
 public class StarterPipeline {
+
+    public interface StarterPipelineOptions extends PipelineOptions {
+
+        /**
+         * By default, this example reads from a public dataset containing the text of King Lear. Set
+         * this option to choose a different input file or glob.
+         */
+        @Description("Path of the file to read from")
+        @Default.String("gs://apache-beam-samples/shakespeare/kinglear.txt")
+        String getInputFile();
+
+        void setInputFile(String value);
+
+        /**
+         * Set this required option to specify where to write the output.
+         */
+        @Description("Path of the file to write to")
+        @Validation.Required
+        String getOutput();
+
+        void setOutput(String value);
+    }
+
+    /**
+     * Concept #2: You can make your pipeline assembly code less verbose by defining your DoFns
+     * statically out-of-line. This DoFn tokenizes lines of text into individual words; we pass it to
+     * a ParDo in the pipeline.
+     */
+    static class ExtractWordsFn extends DoFn<String, String> {
+        private final Counter emptyLines = Metrics.counter(ExtractWordsFn.class, "emptyLines");
+        private final Distribution lineLenDist =
+                Metrics.distribution(ExtractWordsFn.class, "lineLenDistro");
+
+        @ProcessElement
+        public void processElement(@Element String element, OutputReceiver<String> receiver) {
+            lineLenDist.update(element.length());
+            if (element.trim().isEmpty()) {
+                emptyLines.inc();
+            }
+
+            // Split the line into words.
+            String[] words = element.split("[^\\p{L}]+", -1);
+
+            // Output each word encountered into the output PCollection.
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    receiver.output(word);
+                }
+            }
+        }
+    }
+
+    /**
+     * A PTransform that converts a PCollection containing lines of text into a PCollection of
+     * formatted word counts.
+     *
+     * <p>Concept #3: This is a custom composite transform that bundles two transforms (ParDo and
+     * Count) as a reusable PTransform subclass. Using composite transforms allows for easy reuse,
+     * modular testing, and an improved monitoring experience.
+     */
+    public static class CountWords
+            extends PTransform<PCollection<String>, PCollection<KV<String, Long>>> {
+        @Override
+        public PCollection<KV<String, Long>> expand(PCollection<String> lines) {
+
+            // Convert lines of text into individual words.
+            PCollection<String> words = lines.apply(ParDo.of(new ExtractWordsFn()));
+
+            // Count the number of times each word occurs.
+            PCollection<KV<String, Long>> wordCounts = words.apply(Count.perElement());
+
+            return wordCounts;
+        }
+    }
+
+    /**
+     * A SimpleFunction that converts a Word and Count into a printable string.
+     */
+    public static class FormatAsTextFn extends SimpleFunction<KV<String, Long>, String> {
+        @Override
+        public String apply(KV<String, Long> input) {
+            return input.getKey() + ": " + input.getValue();
+        }
+    }
+
+    static void runStarterPipeline(StarterPipelineOptions options) {
+        Pipeline p = Pipeline.create(options);
+
+        p.apply("ReadLines", TextIO.read().from(options.getInputFile()))
+                .apply(new CountWords())
+                .apply(MapElements.via(new FormatAsTextFn()))
+                .apply("WriteCounts", TextIO.write().to(options.getOutput()));
+
+        p.run().waitUntilFinish();
+    }
+
     public static void main(String[] args) {
 
         // from here we get the information from the command line argument
         // By default we return PipelineOptions entity. But we can return our own entity derived from PipelineOtions interfaces --> 'as(...)' method
         // In particular we define the pipeline runner through the options. If none is set, we use the DirectRunner (used to run pipeline locally)
-        PipelineOptions options = PipelineOptionsFactory
+        StarterPipelineOptions options = PipelineOptionsFactory
                 .fromArgs(args)
                 .withValidation()
-                .create();
+                .as(StarterPipelineOptions.class);
 
-        Pipeline p = Pipeline.create(options);
-
-        PCollection<String> upperCaseWords = p.apply(Create.of("Hello", "World"))
-                .apply(MapElements.via(new SimpleFunction<String, String>() {
-                    @Override
-                    public String apply(String input) {
-                        return input.toUpperCase();
-                    }
-                }));
-
-        List<String> expectedResults = Lists.newArrayList("HELLO", "WORLD");
-        PAssert.that(upperCaseWords).containsInAnyOrder(expectedResults);
-
-
-        upperCaseWords
-                .apply(ParDo.of(new DoFn<String, Void>() {
-                    @ProcessElement
-                    public void processElement(ProcessContext c) {
-                        log.info(c.element());
-                    }
-                }));
-
-        p.run();
+        runStarterPipeline(options);
     }
 }
